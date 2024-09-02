3d747f8e2405afed1beb8df8c5506453d76840cc
mobinn <enlighten.ch@gmail.com>
2024-09-02 21:55:59 +0900

    add auto sudo login

--- scripts/start_flir.sh
+++ scripts/start_flir.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 PYTHON_SCRIPT_PATH="/home/enlighten/catkin_ws/src/pure_thermal/scripts/uvc-radiometry.py"
-sudo -E python3 "$PYTHON_SCRIPT_PATH"
+echo "1" | sudo -S -E python3 "$PYTHON_SCRIPT_PATH"
