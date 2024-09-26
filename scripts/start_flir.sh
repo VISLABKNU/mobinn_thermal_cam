122e5b5baadef7e0e996e30e22ece6b28187485b
vislabknu <vislabknu@gmail.com>
2024-09-26 21:49:51 +0900

    thermal_related

--- scripts/start_flir.sh
+++ scripts/start_flir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-PYTHON_SCRIPT_PATH="/home/enlighten/catkin_ws/src/pure_thermal/scripts/uvc-radiometry.py"
+#PYTHON_SCRIPT_PATH="/home/mobinn/catkin_ws/src/pure_thermal/scripts/uvc-radiometry.py"
+PYTHON_SCRIPT_PATH="/home/mobinn/catkin_ws/src/mobinn_thermal_cam/scripts/uvc-radiometry.py"
 echo "1" | sudo -S -E python3 "$PYTHON_SCRIPT_PATH"
